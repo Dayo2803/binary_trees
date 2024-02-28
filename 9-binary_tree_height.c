4e93ff18e71362dc6d1aa60f442f039d5d2aabf6
Seunda <ludivinseunda2@gmail.com>
2024-02-28 13:07:04 +0300

    solve task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,15 +14,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree->left != NULL)
 			left_height = 1 + binary_tree_height(tree->left);
-		else
-			left_height = 0;
 		if (tree->right != NULL)
 			right_height = 1 + binary_tree_height(tree->right);
-		else
-			right_height = 0;
-		if (left_height >= right_height)
-			return (left_height);
-		else
-			return (right_height);
+
+		return (left_height >= right_height) ? left_height : right_height;
 	}
+
+	return 0;
 }
