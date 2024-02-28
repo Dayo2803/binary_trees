a21eee929830b9c900d94edfbb7fdcf92c8ce35a
Seunda <ludivinseunda2@gmail.com>
2024-02-28 13:13:17 +0300

    solve task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -22,17 +22,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
-
 	if (tree)
 	{
-		if (tree->left != NULL)
-			left_height = 1 + binary_tree_height(tree->left);
-		if (tree->right != NULL)
-			right_height = 1 + binary_tree_height(tree->right);
+		size_t l = 0, r = 0;
 
-		return ((left_height >= right_height) ? left_height : right_height);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
 	}
-
 	return (0);
 }
