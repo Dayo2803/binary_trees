60616dcbca094390138519cd70a96671c9c43b26
Seunda <ludivinseunda2@gmail.com>
2024-02-28 11:30:33 +0300

    go back on previous changes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,19 +5,21 @@
  * @parent: The node to which the new_node will be added
  * @value: The value of the new node
  * Return: A pointer to the newly created node or null
-*/
+ */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value){
-    binary_tree_t *new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return (NULL);
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        new_node->left->parent = new_node;
-        parent->left = new_node;
-        return (new_node);
-    }
-    parent->left = new_node;
-    return (new_node);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+
+	if (parent == NULL || new_node == NULL)
+		return (NULL);
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+		parent->left = new_node;
+		return (new_node);
+	}
+	parent->left = new_node;
+	return (new_node);
 }
